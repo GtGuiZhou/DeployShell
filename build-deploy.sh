441ed92677c8f01719f98ac44b2cc8ab23d4f095
gtguizhou <gtguizhou@qq.com>
2019-03-13 15:34:19 +0800

    修正bug

--- build-deploy.sh
+++ build-deploy.sh
@@ -14,7 +14,7 @@ user="www"
 group="www"
 origin="origin" # 部署的生产环境使用的origin
 branch="master" # 部署的生产环境使用的分支
-middle_store_path=$(dirname $(pwd)) # 裸仓库根目录
+middle_store_root_path=$(dirname $(pwd)) # 裸仓库根目录
 produce_store_root_path="/www/wwwroot" # 生产环境根目录
 
 # 接受参数
@@ -47,17 +47,18 @@ fi
 done
 
 # 生产环境目录
-produce_store_path = "${produce_store_root_path}/${item_name}"
+produce_store_path="${produce_store_root_path}/${item_name}"
 
 echo "创建裸仓库中..."
-cd $middle_store_path
+cd $middle_store_root_path
 git init --bare "${item_name}.git"
 chown $user:$group -R $item_path # 修改裸仓库的所属用户
 
 
-echo "克隆生产仓库中..."
+echo "克隆生产仓库..."
+middle_store_path="${middle_store_root_path}/${item_name}.git"
 cd $produce_store_root_path
-git clone $item_path 
+git clone $middle_store_path 
 chown $user:$group -R $produce_store_path # 修改生产环境的所属用户
 
 echo "创建钩子"
